d5d7617f667893b47e826465458af2de731651e4
rose2226 <157970856+rose2226@users.noreply.github.com>
2025-04-25 21:55:48 +0300

    Update app.py

--- app.py
+++ app.py
@@ -139,7 +139,7 @@ st.markdown("""
         border: none;
         padding: 12px 24px;
         border-radius: 10px;
-        width:60%;
+        width:100%;
         margin: 25px auto 0;
         display: block;
         transition: all 0.3s ease;
@@ -184,7 +184,7 @@ st.markdown("""
         font-weight: 200;
         border: 1px solid #cbd5e1;
         padding: 16px 8px;
-        border-radius: 8px;
+        border-radius: 10px;
         cursor: pointer;
         transition: all 0.2s ease;
         margin-top: 10px;
